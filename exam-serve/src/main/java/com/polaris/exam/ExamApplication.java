3f547b295188f7c43916d63b03989a51aca125c0
CNPolaris <42991837+CNPolaris@users.noreply.github.com>
2022-05-02 12:11:15 +0800

    update 添加字符输出

--- exam-serve/src/main/java/com/polaris/exam/ExamApplication.java
+++ exam-serve/src/main/java/com/polaris/exam/ExamApplication.java
@@ -17,5 +17,26 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class ExamApplication {
     public static void main(String[] args) {
         SpringApplication.run(ExamApplication.class, args);
+
+        System.out.println("" +
+                "  _____            _                  _       \n" +
+                " |  __ \\          | |                (_)      \n" +
+                " | |__) |   ___   | |   __ _   _ __   _   ___ \n" +
+                " |  ___/   / _ \\  | |  / _` | | '__| | | / __|\n" +
+                " | |      | (_) | | | | (_| | | |    | | \\__ \\\n" +
+                " |_|       \\___/  |_|  \\__,_| |_|    |_| |___/\n" +
+                "                                              \n" +
+                "                                             ");
+
+        System.out.println("" +
+                "   ('-.   ) (`-.         ('-.      _   .-')    \n" +
+                " _(  OO)   ( OO ).      ( OO ).-. ( '.( OO )_  \n" +
+                "(,------. (_/.  \\_)-.   / . --. /  ,--.   ,--.)\n" +
+                " |  .---'  \\  `.'  /    | \\-.  \\   |   `.'   | \n" +
+                " |  |       \\     /\\  .-'-'  |  |  |         | \n" +
+                "(|  '--.     \\   \\ |   \\| |_.'  |  |  |'.'|  | \n" +
+                " |  .--'    .'    \\_)   |  .-.  |  |  |   |  | \n" +
+                " |  `---.  /  .'.  \\    |  | |  |  |  |   |  | \n" +
+                " `------' '--'   '--'   `--' `--'  `--'   `--' ");
     }
 }
